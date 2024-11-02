046ca32eeb4528cb6a418d650673e6819b6830e9
Vanny2402 <79197656+Vanny2402@users.noreply.github.com>
2024-11-02 16:18:16 +0700

    'starter'

---
+++ src/test/java/selenium_java/BrowserTest.java
@@ -0,0 +1,31 @@
+package selenium_java;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BrowserTest {
+
+	public static void main(String[] args) throws InterruptedException{
+//		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		System.out.println(WebDriverManager.chromedriver().getDownloadedDriverPath());
+
+		driver.get("https://www.google.com/");
+		WebElement textbox= driver.findElement(By.id("APjFqb"));
+		textbox.sendKeys("Automation testing");
+		
+		Thread.sleep(3500);
+		driver.close();
+		
+//		String prjectPath=System.getProperty("user.dir");
+//		System.out.println("Project Path: "+prjectPath);
+//		WebDriver driver = new FirefoxDriver();
+//		driver.get("https://www.selenium.dev/");
+//		Thread.sleep(2000);
+//		driver.quit();
+	}
+}
